b260b395dc5e19e5aad6c4c9f0245001ce31859a
Tree Greens <naveenkumarks27@gmail.com>
2026-01-28 21:30:12 +0530

    Fix Vercel deployment (remove vercel.json, fix base)

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./app/App";
+import "./styles/index.css"; // IMPORTANT
 
-  import { createRoot } from "react-dom/client";
-  import App from "./app/App.tsx";
-  import "./styles/index.css";
-
-  createRoot(document.getElementById("root")!).render(<App />);
-  
+ReactDOM.createRoot(
+  document.getElementById("root")!
+).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
