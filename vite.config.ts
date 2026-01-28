0adffea8b489bc979d0887bf3737640c967f7021
Tree Greens <naveenkumarks27@gmail.com>
2026-01-28 18:58:55 +0530

    Vercel deploy fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
-// vite.config.js
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 import path from 'path'
+import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
-    alias: { '@': path.resolve(__dirname, './src') },
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
   },
-  base: './', // important! ensures relative paths for JS/CSS/assets
+  base: './', // ✅ must have this for Vercel
 })
